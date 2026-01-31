d807918bb1810a313e2fa6036556d243bbf72605
Sharon Moegi <sharamoegi@gmail.com>
2026-01-31 17:55:35 +0300

    Retrieve user input

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+//1. DOM ELEMENTS
+const birthYear = document.getElementById('birthyear');
+const birthMonth = document.getElementById('birthmonth');
+const birthDay = document.getElementById('birthday');
+const gender = document.getElementById('gender');
 
